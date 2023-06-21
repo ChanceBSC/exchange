9348756ad9ec4a28b7f5a0410473a06093258eab
Obinna Okolo <oojoseph67@gmail.com>
2023-06-21 12:40:54 +0100

    header tag added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import { Toaster } from "react-hot-toast";
-
+import Head from "next/head";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
@@ -11,6 +11,9 @@ const activeChain = "binance-testnet";
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
+      <Head>
+        <title>Chance Exchange</title>
+      </Head>
       <Component {...pageProps} />
       <Toaster></Toaster>
     </ThirdwebProvider>
